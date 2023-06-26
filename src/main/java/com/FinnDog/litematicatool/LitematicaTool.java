8c9910f4feb50f6fff102ffaaf402d122b4468b3
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2023-06-26 19:01:14 +0100

    updated to 1.19.3-4 updated texture

--- src/main/java/com/FinnDog/litematicatool/LitematicaTool.java
+++ src/main/java/com/FinnDog/litematicatool/LitematicaTool.java
@@ -2,14 +2,16 @@ package com.FinnDog.litematicatool;
 
 import com.FinnDog.litematicatool.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LitematicaTool implements ModInitializer {
 
     public static final String MOD_ID = "litematicatool";
+    public static final Logger LOGGER = LoggerFactory.getLogger("litematicatool");
 
     @Override
     public void onInitialize() {
-        ModItems.registerItems();
-
+        ModItems.registerModItems();
     }
 }
--- src/main/java/com/FinnDog/litematicatool/registry/ModItems.java
+++ src/main/java/com/FinnDog/litematicatool/registry/ModItems.java
@@ -1,17 +1,36 @@
 package com.FinnDog.litematicatool.registry;
 
 import com.FinnDog.litematicatool.LitematicaTool;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
-    public static final Item THE_STICK = new Item(new Item.Settings().group(ItemGroup.TOOLS));
+    public static final Item THE_STICK = registerItem("the_stick",
+            new Item(new FabricItemSettings()));
 
-    public static void registerItems() {
-        Registry.register(Registry.ITEM, new Identifier(LitematicaTool.MOD_ID, "the_stick"), THE_STICK);
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(LitematicaTool.MOD_ID, name), item);
+    }
+
+    public static void addItemsToItemGroup() {
+        addToItemGroup(ItemGroups.TOOLS, THE_STICK);
+    }
+
+    private static void addToItemGroup(ItemGroup group, Item item) {
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
+
+    public static void registerModItems() {
+        LitematicaTool.LOGGER.info("Registering Mod Items for " + LitematicaTool.MOD_ID);
+
+        addItemsToItemGroup();
     }
 }
 
