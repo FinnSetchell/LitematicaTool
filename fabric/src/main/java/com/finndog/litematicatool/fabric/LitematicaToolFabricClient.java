449605527fcac8497c40e9f0ea20afa1380473c6
finnsetchell <finn@setchell.org>
2026-07-06 20:30:36 +0200

    port to jared multiloader (per-loader item registration, drop includeInternal, fix publishMods jar, mc version 26.2)

--- fabric/src/main/java/com/finndog/litematicatool/fabric/LitematicaToolFabricClient.java
+++ fabric/src/main/java/com/finndog/litematicatool/fabric/LitematicaToolFabricClient.java
@@ -1,6 +1,5 @@
 package com.finndog.litematicatool.fabric;
 
-import com.finndog.litematicatool.CommonClass;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.minecraft.network.chat.Component;
@@ -9,7 +8,7 @@ public class LitematicaToolFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ItemTooltipCallback.EVENT.register((stack, tooltipContext, tooltipType, lines) -> {
-            if (stack.getItem() == CommonClass.THE_STICK) {
+            if (stack.getItem() == LitematicaToolFabric.THE_STICK) {
                 lines.add(Component.translatable("item.litematicatool.the_stick.tooltip"));
                 lines.add(Component.translatable("item.litematicatool.the_stick.tooltip.line2"));
                 lines.add(Component.translatable("item.litematicatool.the_stick.tooltip.line3"));
---
+++ neoforge/src/main/java/com/finndog/litematicatool/neoforge/client/LitematicaToolNeoForgeClient.java
@@ -0,0 +1,21 @@
+package com.finndog.litematicatool.neoforge.client;
+
+import com.finndog.litematicatool.Constants;
+import com.finndog.litematicatool.neoforge.LitematicaToolNeoForge;
+import net.minecraft.network.chat.Component;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
+
+@EventBusSubscriber(modid = Constants.MOD_ID, value = Dist.CLIENT)
+public class LitematicaToolNeoForgeClient {
+    @SubscribeEvent
+    public static void onItemTooltip(ItemTooltipEvent event) {
+        if (event.getItemStack().getItem() == LitematicaToolNeoForge.THE_STICK.get()) {
+            event.getToolTip().add(Component.translatable("item.litematicatool.the_stick.tooltip"));
+            event.getToolTip().add(Component.translatable("item.litematicatool.the_stick.tooltip.line2"));
+            event.getToolTip().add(Component.translatable("item.litematicatool.the_stick.tooltip.line3"));
+        }
+    }
+}
